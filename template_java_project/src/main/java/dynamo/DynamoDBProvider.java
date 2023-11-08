40b3e27dc844c2c391596c673c93f9dea6b1e2d3
jenncrdn <crdn_jenn@yahoo.com>
2023-11-08 13:41:40 -0600

    fixing credentials

--- template_java_project/src/main/java/dynamo/DynamoDBProvider.java
+++ template_java_project/src/main/java/dynamo/DynamoDBProvider.java
@@ -10,9 +10,9 @@ import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 public class DynamoDBProvider {
     private static final String dynamoDbEndpoint = "https://dynamodb.us-west-2.amazonaws.com";
 
-    private static final String awsAccessKey = "AKIAV3IWUTHELU3JCVVV";
+//    private static final String awsAccessKey = ;
 
-    private static final String awsSecretKey = "Cv7uYEQrP4HPaQNo5CtlNxMEqDoXZWkJSZPySdW+";
+//    private static final String awsSecretKey = ;
 
     public static AmazonDynamoDB getAmazonDynamoDb() {
         return AmazonDynamoDBClientBuilder.standard()
